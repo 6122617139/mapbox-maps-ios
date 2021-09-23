dcb7d4a84f8188a7c469422137154239d3e357e1
Jordan Kiley <jordan.kiley@mapbox.com>
2021-09-23 10:41:16 -0400

    Add convenience init

--- Sources/MapboxMaps/Foundation/Extensions/Core/OfflineRegionGeometryDefinition.swift
+++ Sources/MapboxMaps/Foundation/Extensions/Core/OfflineRegionGeometryDefinition.swift
@@ -1,6 +1,13 @@
 @_implementationOnly import MapboxCoreMaps_Private
+import MapboxCommon
 
 extension MapboxCoreMaps.OfflineRegionGeometryDefinition {
+
+    convenience init(styleURL: String, geometry: Turf.Geometry, minZoom: Double, maxZoom: Double, pixelRatio: Float, glyphsRasterizationMode: GlyphsRasterizationMode) {
+        let commonGeometry = MapboxCommon.Geometry(geometry: geometry)
+        self.init(styleURL: styleURL, geometry: commonGeometry, minZoom: minZoom, maxZoom: maxZoom, pixelRatio: pixelRatio, glyphsRasterizationMode: glyphsRasterizationMode)
+    }
+
     /// The geometry that defines the boundary of the offline region.
     public var geometry: Turf.Geometry? {
         get {
