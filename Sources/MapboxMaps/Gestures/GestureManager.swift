e5703cd8383e56ce84d4d900db84699d24f333f4
Nishant Karajgikar <nishant.karajgikar@mapbox.com>
2021-09-23 21:23:08 -0400

    Redesign tap architecture for annotations

--- Sources/MapboxMaps/Gestures/GestureManager.swift
+++ Sources/MapboxMaps/Gestures/GestureManager.swift
@@ -63,7 +63,7 @@ public final class GestureManager: GestureHandlerDelegate {
     public var quickZoomGestureRecognizer: UIGestureRecognizer {
         return quickZoomGestureHandler.gestureRecognizer
     }
-    
+
     /// The gesture recognizer for the single tap gesture
     public var singleTapGestureRecognizer: UIGestureRecognizer {
         return singleTapGestureHandler.gestureRecognizer
